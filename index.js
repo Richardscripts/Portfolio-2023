07feb1156cddbe21cebcdb4c1f204aff3b746b2c
unknown <richardcarmenscott@gmail.com>
2021-01-03 18:54:15 -0500

    fix: fixed animation dead links

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ function myFunction() {
       'hidden topScrollButton';
   }
   if (
-    document.body.scrollTop > 700 ||
-    document.documentElement.scrollTop > 700
+    document.body.scrollTop > 600 ||
+    document.documentElement.scrollTop > 600
   ) {
     document.getElementById('first-product-info').className =
       'first-product-info fade';
   }
   if (
-    document.body.scrollTop > 1150 ||
-    document.documentElement.scrollTop > 1150
+    document.body.scrollTop > 1050 ||
+    document.documentElement.scrollTop > 1050
   ) {
     document.getElementById('second-product-info').className =
       'second-product-info fade';
   }
 
   if (
-    document.body.scrollTop > 1700 ||
-    document.documentElement.scrollTop > 1700
+    document.body.scrollTop > 1600 ||
+    document.documentElement.scrollTop > 1600
   ) {
     document.getElementById('third-product-info').className =
       'first-product-info fade';
