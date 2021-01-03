5746d0871c596984035c935c6663a943e925af49
unknown <richardcarmenscott@gmail.com>
2021-01-03 17:05:30 -0500

    fix: fixed animations and scroll top button

--- index.js
+++ index.js
@@ -2,20 +2,31 @@ window.addEventListener('scroll', () => myFunction());
 
 function myFunction() {
   if (
+    document.body.scrollTop >= 1200 ||
+    document.documentElement.scrollTop >= 1200
+  ) {
+    document.getElementById('topScrollButton').className =
+      'topScrollButton bounceInUp';
+  } else if (
+    document.body.scrollTop <= 600 ||
+    document.documentElement.scrollTop <= 600
+  ) {
+    document.getElementById('topScrollButton').className =
+      'hidden topScrollButton';
+  }
+  if (
     document.body.scrollTop > 700 ||
     document.documentElement.scrollTop > 700
   ) {
     document.getElementById('first-product-info').className =
-      'first-product-info rubberBand';
-    document.getElementById('topScrollButton').className =
-      'topScrollButton fade';
+      'first-product-info fade';
   }
   if (
     document.body.scrollTop > 1150 ||
     document.documentElement.scrollTop > 1150
   ) {
     document.getElementById('second-product-info').className =
-      'second-product-info rubberBand';
+      'second-product-info fade';
   }
 
   if (
@@ -23,6 +34,6 @@ function myFunction() {
     document.documentElement.scrollTop > 1700
   ) {
     document.getElementById('third-product-info').className =
-      'first-product-info rubberBand';
+      'first-product-info fade';
   }
 }
