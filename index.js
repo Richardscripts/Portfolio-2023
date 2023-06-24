affc7ac0321dc3ffdda89627dee1b81eb155c196
Scott, Richard (Contractor) <Richard_Scott@comcast.com>
2023-06-24 10:59:59 -0400

    feat: added more animations

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ function onScrollEffects() {
     document.getElementById('topScrollButton').className =
       'hidden topScrollButton';
   }
+
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    document.getElementById('drawing-image').className =
+      'drawing-image fadeOut';
+  }
+
   if (
     document.body.scrollTop > 475 ||
     document.documentElement.scrollTop > 475
@@ -22,6 +31,8 @@ function onScrollEffects() {
     document.getElementById('first-product-info').className =
       'first-product-info fade';
   }
+
+  console.log(document.documentElement.scrollTop);
   if (
     document.body.scrollTop > 950 ||
     document.documentElement.scrollTop > 950
